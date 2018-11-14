488fa50ce7ecf5f50ba319ebb8e09b495df54933
JohnT42 <j.tran24@hotmail.com>
2018-11-14 13:55:00 +0000

    3rd commit, includes feature to populate the account table

---
+++ src/main/java/com/qa/persistance/domain/Accounts.java
@@ -0,0 +1,50 @@
+package com.qa.persistance.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Accounts {
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Column(length = 100)
+	private String firstName;
+	
+	@Column(length = 100)
+	private String lastName;
+	
+	@Column(length = 50)
+	private String accountNum;
+	
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getAccountNum() {
+		return accountNum;
+	}
+
+	public void setAccountNum(String accountNum) {
+		this.accountNum = accountNum;
+	}
+
+	
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO ACCOUNTS (id, accountnum, firstname, lastname)VALUES (1,'1234','John', 'Doe');
+INSERT INTO ACCOUNTS (id, accountnum, firstname, lastname)VALUES (2, '1235','Jane', 'Doe');
+INSERT INTO ACCOUNTS (id, accountnum, firstname, lastname)VALUES (3,'1236','Jim', 'Taylor');
